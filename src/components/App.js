09909faa6e9beb5cd11f283b0824cefa91c80db9
DESKTOP-9LV2PBB\GPS <iankhor2001@hotmail.com>
2021-09-17 19:16:34 +0800

    App.js - setup Router, added Navbar

---
+++ src/components/App.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from 'react-router-dom';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+// import Container from 'react-bootstrap/Container';
+
+import './App.css';
+
+export class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            unit: 'metric',
+            navbarToggle: false,
+        };
+        this.handleUnitChange = this.handleUnitChange.bind(this);
+    };
+
+    render() {
+        return (
+            <Router>
+                <div className={`main bg-faded ${this.state.group}`}>
+                    <Navbar bg="light">
+                        <div className="container">
+                            <Navbar.Brand className="navbar-brand">Weather Forecast</Navbar.Brand>
+                            <Navbar.Toggle aria-controls="navbar-content"/>
+                            <Navbar.Collapse id="navbar-content">
+                                <Nav className="me-auto">
+                                    <Nav.Link as={Link} eventKey="home" to="/">Home</Nav.Link>
+                                    <Nav.Link as={Link} eventKey="forecast" to="/forecast">Forecast</Nav.Link>
+                                </Nav>
+                                <span className="navbar-text ml-auto">Ian Khor</span>
+                            </Navbar.Collapse>
+                        </div>
+                    </Navbar>
+                </div>
+                <Switch>
+                    <Route exact path="/">
+                        Home
+                    </Route>
+                    <Route path="/forecast">
+                        Forecast
+                    </Route>
+                </Switch>
+                
+            </Router>
+        )
+    }
+
+    handleUnitChange(unit) {
+        this.setState({
+            unit: unit,
+        })
+    }
+}
