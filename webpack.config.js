21e2c5513a8a4a4a337920920f5555836aaf6a80
DESKTOP-9LV2PBB\GPS <iankhor2001@hotmail.com>
2021-09-17 18:06:11 +0800

    removed react-router, added react-router-dom, added 'style-loader' to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -17,11 +17,11 @@ module.exports = {
                 use: {
                     loader: 'babel-loader',
                 },
-            },
-            {
+            }, {
                 test: /\.scss$/,
                 use: [
                     MiniCssExtractPlugin.loader,
+                    'style-loader',
                     'css-loader',
                     'sass-loader',
                 ]
