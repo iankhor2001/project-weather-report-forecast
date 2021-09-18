5881104ec0f94fc30252357cb4886cf5ee816e50
DESKTOP-9LV2PBB\GPS <iankhor2001@hotmail.com>
2021-09-19 02:33:15 +0800

    Added TodayWeatherDisplay Component & Imgs

---
+++ src/components/TodayWeatherDisplay.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class TodayWeatherDisplay extends React.Component {
+    static propTypes = {
+        masking: PropTypes.bool,
+        group: PropTypes.string,
+        description: PropTypes.string,
+        temp: PropTypes.number,
+        unit: PropTypes.string,
+    };
+
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        return (
+            <div className={`weather-display ${this.props.masking ? 'masking' : ''}`}>
+                <img src={require(`images/w-${this.props.group}.png`).default}/>
+                <p className='description'>{this.props.description}</p>&nbsp;
+                <h1 className='temp'>
+                    <span className='display-3'>{this.props.temp.toFixed(0)}&ordm;</span>
+                    &nbsp;
+                    {(this.props.unit === 'metric') ? 'C' : 'F'}
+                </h1>
+            </div>
+        )
+    }
+}
