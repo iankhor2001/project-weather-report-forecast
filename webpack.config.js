fbade4f1674a34920cbf9cadb558f27aa3054a17
DESKTOP-9LV2PBB\GPS <iankhor2001@hotmail.com>
2021-09-19 02:29:28 +0800

    package - installed url-loader & file-loader webpack.config.js - added alias for images, components, api & implement url-loader

--- webpack.config.js
+++ webpack.config.js
@@ -2,28 +2,66 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
+    resolve: {
+        alias: {
+            images: path.resolve(__dirname, 'dist/images'),
+            components: path.resolve(__dirname, 'src/components'),
+            api: path.resolve(__dirname, 'src/api'),
+        }
+    },
+    entry: {
+        index: './src/index.js',
+        vendor: ['react', 'react-dom']
+    },
     output: {
         path: path.join(__dirname, '/dist'),
-        filename: 'index.bundle.js',
+        filename: '[name].bundle.js',
     },
     devServer: {
         port: 8080,
     },
     module: {
         rules: [
-            {
+            {  // JS & JSX
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
                 use: {
                     loader: 'babel-loader',
                 },
-            }, {
+            }, {  // SASS
                 test: /\.scss$/,
                 use: [
                     MiniCssExtractPlugin.loader,
                     'style-loader',
-                    'css-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            url: false
+                        }
+                    },
                     'sass-loader',
+                ]
+            }, {  // CSS
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options : {
+                            url: false
+                        }
+                    }
+                ]
+            }, {  // PNG & JPG & GIF
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit:8192,
+                            url: false,
+                        }
+                    }
                 ]
             }
         ]
