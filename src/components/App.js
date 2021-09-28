ddd1b587466dee181c5073b41083e308c4a8eeb3
DESKTOP-9LV2PBB\GPS <iankhor2001@hotmail.com>
2021-09-29 01:38:55 +0800

    App, Today .js - moved all states&functions of Today to App for implementing OneCallApi

--- src/components/App.js
+++ src/components/App.js
@@ -5,9 +5,11 @@ import {
     Route,
     Link
 } from 'react-router-dom';
+
+import {getTodayWeather, getGeolocationWeather, cancelWeather} from 'api/open-weather-map';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-// import Container from 'react-bootstrap/Container';
+import Modal from 'react-bootstrap/Modal';
 
 import Today from './Today';
 import { Footer } from  './StaticComponent';
@@ -15,13 +17,33 @@ import './App.css';
 import './Theme.css';
 
 export class App extends React.Component {
+    static getInitWeatherState() { 
+        return {
+            city: 'na',
+            code: -1,
+            group: 'na',
+            description: 'N/A',
+            temp: NaN,
+        };
+    }
+
     constructor(props) {
         super(props);
         this.state = {
+            ...App.getInitWeatherState(), 
             unit: 'metric',
             navbarToggle: false,
+            currentLocation: 'naCL',
+            loading: true,
+            masking: true,
         };
+
         this.handleUnitChange = this.handleUnitChange.bind(this);
+        this.handleFormQuery = this.handleFormQuery.bind(this);
+        this.handleGetGeolocationWeather = this.handleGetGeolocationWeather.bind(this);
+        this.handleCloseWeatherError = this.handleCloseWeatherError.bind(this);
+        this.handleShowWeatherError = this.handleShowWeatherError.bind(this);
+        this.handleGetGeolocationWeather = this.handleGetGeolocationWeather.bind(this);
     };
 
     render() {
@@ -45,14 +67,32 @@ export class App extends React.Component {
                 <div className="weather-body">
                     <Switch>
                         <Route exact path="/">
-                            <Today unit={this.state.unit} onUnitChange={this.handleUnitChange} />
+                            <Today 
+                                {...this.state}
+                                showGetWeatherError={this.state.showGetWeatherError}
+                                handleFormQuery={this.handleFormQuery}
+                                handleGetGeolocationWeather={this.handleGetGeolocationWeather}
+                            />
                         </Route>
                         <Route path="/forecast">
                             Forecast
                         </Route>
                     </Switch>
                 </div>
-                
+                <Modal 
+                    show={this.state.showGetWeatherError} 
+                    onHide={this.handleCloseWeatherError}
+                >
+                    <div className={'bg-danger text-white text-center border border-light rounded'}>
+                        <Modal.Header closeButton>
+                            <Modal.Title>Error retrieving weather!</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            Error getting weather<br/>Displaying default location
+                        </Modal.Body>
+                    </div>
+                    
+                </Modal>
                 <Footer />
             </Router>
         )
@@ -61,6 +101,117 @@ export class App extends React.Component {
     handleUnitChange(unit) {
         this.setState({
             unit: unit,
+        })
+    }
+
+    componentDidMount() {
+        this.getGeolocationWeather(this.state.unit);
+    }
+
+    componentWillUnmount() {
+        this.state.loading && cancelWeather();
+    }
+
+    getGeolocationWeather(unit) {
+        this.setState({
+            loading: true,
+            masking: true,
+        }, () => {
+            console.log("Geolocation weather")
+            window.navigator.geolocation.getCurrentPosition(
+                (returnPosition) => {
+                    let lat = returnPosition.coords.latitude;
+                    let lon = returnPosition.coords.longitude;
+
+                    getGeolocationWeather(lat, lon, unit).then((weather) => {
+                        console.log('Received Geolocation Weather Information');
+                        this.setWeatherInformations(weather, unit);
+                    }).catch(err => {
+                        console.error('Error getting weather, displaying default location\n', err);
+                        this.handleShowWeatherError();
+                        this.setWeatherInformations(null, unit, err, 'Hsinchu');
+                    });
+                },
+                (error) => {
+                    console.log('Error getting weather, displaying default location\n', error);
+                    this.handleShowWeatherError();
+                    setWeatherInformations(null, unit, error, (this.state.city==='na')?'Hsinchu':this.state.city )
+                },
+            );
+        });
+
+        setTimeout(() => {
+            this.setState({
+                masking: false
+            });
+        }, 600);
+    }
+
+    getWeather(city, unit) { 
+        console.log('Getting Weather from '+ String(city));
+        this.setState({
+            loading: true,
+            masking: true,
+            city: city
+        }, () => {
+            getTodayWeather(city, unit).then(weather => {
+                console.log('Received Information');
+                this.setWeatherInformations(weather, unit);
+            }).catch(err => {
+                console.error('Error getting weather, displaying default location\n', err);
+                this.handleShowWeatherError();
+                this.setWeatherInformations(null, unit, err);
+            });
+        });
+
+        setTimeout(() => {
+            this.setState({
+                masking: false
+            });
+        }, 600);
+    }
+
+    setWeatherInformations(weather, unit, err=null, city=null) {
+        if (err) {
+            this.setState({
+                ...App.getInitWeatherState(unit),
+                loading: false,
+            }, () => {
+                this.notifyUnitChange(unit);
+                this.getWeather( city?city:this.state.currentLocation , this.state.unit);
+            });
+        } else {
+            console.log(weather);
+            this.setState({
+                ...weather,
+                loading: false,
+            }, () => this.notifyUnitChange(unit));
+        }
+        
+    }
+
+    handleFormQuery(city, unit) {
+        this.getWeather(city, unit);
+    }
+
+    handleGetGeolocationWeather(unit) {
+        this.getGeolocationWeather(unit);
+    }
+
+    notifyUnitChange(unit) {
+        (this.state.units !== unit) &&
+            this.handleUnitChange(unit);    
+    }
+
+    handleShowWeatherError() {
+        this.setState({
+            showGetWeatherError: true
+        })
+    }
+
+    handleCloseWeatherError() {
+        this.setState({
+            showGetWeatherError: false
         })
     }
 }
--- src/components/Today.js
+++ src/components/Today.js
@@ -1,176 +1,27 @@
 import React from 'react';
-import {getTodayWeather, getGeolocationWeather, cancelWeather} from 'api/open-weather-map';
-import Modal from 'react-bootstrap/Modal';
 
-import TodayWeatherDisplay, { ShowGetWeatherError } from './TodayWeatherDisplay';
+import TodayWeatherDisplay from './TodayWeatherDisplay';
 import TodayWeatherForm from './TodayWeatherForm';
-import Button from 'react-bootstrap/Button';
 
 export default class Today extends React.Component {
-    static getInitWeatherState() { 
-        return {
-            city: 'na',
-            code: -1,
-            group: 'na',
-            description: 'N/A',
-            temp: NaN,
-        };
-    }
-
     constructor(props) {
         super(props);
-
-        this.state = {
-            ...Today.getInitWeatherState(),
-            currentLocation: 'naCL',
-            loading: true,
-            masking: true,
-            showGetWeatherError: false,
-        };
-
-        this.handleFormQuery = this.handleFormQuery.bind(this);
-        this.handleCloseWeatherError = this.handleCloseWeatherError.bind(this);
-        this.handleShowWeatherError = this.handleShowWeatherError.bind(this);
-        this.handleGetGeolocationWeather = this.handleGetGeolocationWeather.bind(this);
     }
 
     render() {
         return (
-            <div className={`today weather-bg ${this.state.group}`}>
-                <div className={`mask ${this.state.masking ? 'masking' : ''}`}>
+            <div className={`today weather-bg ${this.props.group}`}>
+                <div className={`mask ${this.props.masking ? 'masking' : ''}`}>
                     <TodayWeatherForm 
-                        city={this.state.city} 
+                        city={this.props.city} 
                         unit={this.props.unit} 
-                        onQuery={this.handleFormQuery}
-                        getGeoWeather={this.handleGetGeolocationWeather}
+                        onQuery={this.props.handleFormQuery}
+                        getGeoWeather={this.props.handleGetGeolocationWeather}
                     />
-                    <TodayWeatherDisplay {...this.state}/>
-                    <Modal 
-                        show={this.state.showGetWeatherError} 
-                        onHide={this.handleCloseWeatherError}
-                    >
-                        <div className={'bg-danger text-white text-center border border-light rounded'}>
-                            <Modal.Header closeButton>
-                                <Modal.Title>Error retrieving weather!</Modal.Title>
-                            </Modal.Header>
-                            <Modal.Body>
-                                Error getting weather<br/>Displaying default location
-                            </Modal.Body>
-                        </div>
-                        
-                    </Modal>
+                    <TodayWeatherDisplay {...this.props}/>
+                    
                 </div>
             </div>
         )
-    }
-
-    componentDidMount() {
-        this.getGeolocationWeather(this.props.unit)
-    }
-
-    componentWillUnmount() {
-        this.state.loading && cancelWeather();
-    }
-
-    getGeolocationWeather(unit) {
-        this.setState({
-            loading: true,
-            masking: true,
-        }, () => {
-            window.navigator.geolocation.getCurrentPosition(
-                (returnPosition) => {
-                    let lat = returnPosition.coords.latitude;
-                    let lon = returnPosition.coords.longitude;
-
-                    getGeolocationWeather(lat, lon, this.props.unit).then((weather) => {
-                        console.log('Received Geolocation Weather Information');
-                        this.setWeatherInformations(weather, unit);
-                    }).catch(err => {
-                        console.error('Error getting weather, displaying default location\n', err);
-                        this.handleShowWeatherError();
-                        this.setWeatherInformations(null, unit, err, 'Hsinchu');
-                    });
-                },
-                (error) => {
-                    console.log('Error getting weather, displaying default location\n', error);
-                    this.handleShowWeatherError();
-                    setWeatherInformations(weather, unit, error, (this.state.city==='na')?'Hsinchu':this.state.city )
-                },
-            );
-        });
-
-        setTimeout(() => {
-            this.setState({
-                masking: false
-            });
-        }, 600);
-    }
-
-    getWeather(city, unit) { 
-        console.log('Getting Weather from '+ String(city));
-        this.setState({
-            loading: true,
-            masking: true,
-            city: city
-        }, () => {
-            getTodayWeather(city, unit).then(weather => {
-                console.log('Received Information');
-                this.setWeatherInformations(weather, unit);
-            }).catch(err => {
-                console.error('Error getting weather, displaying default location\n', err);
-                this.handleShowWeatherError();
-                this.setWeatherInformations(null, unit, err);
-            });
-        });
-
-        setTimeout(() => {
-            this.setState({
-                masking: false
-            });
-        }, 600);
-    }
-
-    setWeatherInformations(weather, unit, err=null, city=null) {
-        if (err) {
-            this.setState({
-                ...Today.getInitWeatherState(unit),
-                loading: false,
-            }, () => {
-                this.notifyUnitChange(unit);
-                this.getWeather( city?city:this.state.currentLocation , this.props.unit);
-            });
-        } else {
-            console.log(weather);
-            this.setState({
-                ...weather,
-                loading: false,
-            }, () => this.notifyUnitChange(unit));
-        }
-        
-    }
-
-    handleFormQuery(city, unit) {
-        this.getWeather(city, unit);
-    }
-
-    handleGetGeolocationWeather(unit) {
-        this.getGeolocationWeather(unit);
-    }
-
-    notifyUnitChange(unit) {
-        (this.props.units !== unit) &&
-            this.props.onUnitChange(unit);    
-    }
-
-    handleShowWeatherError() {
-        this.setState({
-            showGetWeatherError: true
-        })
-    }
-
-    handleCloseWeatherError() {
-        this.setState({
-            showGetWeatherError: false
-        })
     }
 }
